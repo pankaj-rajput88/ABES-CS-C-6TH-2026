7c62c4bb3a397f2ad15e5e88d04dfb0422706242
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2026-02-12 12:25:54 +0530

    async fun of fs

--- day_02/readAndWriteFile.js
+++ day_02/readAndWriteFile.js
@@ -1,27 +1,19 @@
 import fs from 'fs';
 
-const appendFileSync = (path, data) => {
+export const appendFileSync = (path, data) => {
     try {
-        fs.appendFileSync(file, data);
+        fs.appendFileSync(path, data);
         console.log("Data has been appended successfully.");
     } catch (error) {
         console.log("unable to append file");
     }
 }
-appendFileSync("./example.txt", "This data has been appended through sync fun of fs");
-
-
-
-
-
-
 
 export const writeFileSync = (path, data) => {
     try {
         fs.writeFileSync(path, data);
         console.log("Data has been successfully written...");
     } catch (error) {
-        4
         console.log("unable to write data");
     }
 }
